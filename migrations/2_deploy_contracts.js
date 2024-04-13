6de936c64378f17d34757614b41f0d363d3a4373
xxianhuii <keexianhui@gmail.com>
2024-04-13 17:25:33 +0800

    Created skeleton for acceptance voting

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,14 @@
-const Post = artifacts.require("Post");
-const User = artifacts.require("User");
-const MisinformationReport = artifacts.require("MisinformationReport");
+const Post = artifacts.require('Post');
+const User = artifacts.require('User');
+const MisinformationReport = artifacts.require('MisinformationReport');
+const Token = artifacts.require('Token');
+const AcceptanceVoting = artifacts.require('AcceptanceVoting');
 
-module.exports = function(deployer) {
-  deployer.deploy(Post)
+module.exports = function (deployer) {
+  deployer
+    .deploy(Post)
     .then(() => deployer.deploy(User))
-    .then(() => deployer.deploy(MisinformationReport, Post.address));
+    .then(() => deployer.deploy(MisinformationReport, Post.address))
+    .then(() => deployer.deploy(Token))
+    .then(() => deployer.deploy(AcceptanceVoting, 48));
 };

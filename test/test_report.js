6ffb14ba91ebd64b819f838ac9d5e3632573d98b
gwajoon <hengguojun25@gmail.com>
2024-04-17 16:36:12 +0800

    feat: Add test case for insufficient ethers sent during add report

--- test/test_report.js
+++ test/test_report.js
@@ -1,5 +1,8 @@
 const truffleAssert = require("truffle-assertions");
+const BigNumber = require("bignumber.js");
 var assert = require("assert");
+
+const oneEth = new BigNumber(1000000000000000000); // 1 eth
 
 const Report = artifacts.require("../contracts/Report.sol");
 const Post = artifacts.require("../contracts/Post.sol");
@@ -20,7 +23,7 @@ contract("Report Test", (accounts) => {
     let addReport = await reportInstance.addReport(
       0,
       "This is a test justification",
-      { from: accounts[0] }
+      { from: accounts[0], value: oneEth.dividedBy(10000) }
     );
 
     assert.notStrictEqual(addReport, undefined, "Failed to add report");
@@ -33,24 +36,58 @@ contract("Report Test", (accounts) => {
     });
 
     truffleAssert.reverts(
-      reportInstance.addReport(0, "", { from: accounts[0] }),
+      reportInstance.addReport(0, "This is a test justification", {
+        from: accounts[0],
+        value: oneEth.dividedBy(1000000),
+      }),
+      "Insufficient ether sent for adding report"
+    );
+
+    truffleAssert.reverts(
+      reportInstance.addReport(0, "", {
+        from: accounts[0],
+        value: oneEth.dividedBy(10000),
+      }),
       "Report justification cannot be empty"
     );
   });
 
   it("Update Report", async () => {
     await postInstance.addPost("Test Title", "Test content", {
-      from: accounts[0]
+      from: accounts[0],
     });
 
     await reportInstance.addReport(0, "This is a test justification", {
-      from: accounts[0]
+      from: accounts[0],
+      value: oneEth.dividedBy(10000),
     });
 
     let updateReport = await reportInstance.updateReport(0, 1, {
-      from: accounts[0]
+      from: accounts[0],
     });
 
     truffleAssert.eventEmitted(updateReport, "ReportUpdated");
+  });
+
+  it("Delete Report", async () => {
+    await postInstance.addPost("Test Title", "Test content", {
+      from: accounts[0],
+    });
+
+    await reportInstance.addReport(0, "This is a test justification", {
+      from: accounts[0],
+      value: oneEth.dividedBy(10000),
+    });
+
+    let deleteReport = await reportInstance.deleteReport(0, {
+      from: accounts[0],
+    });
+
+    truffleAssert.eventEmitted(deleteReport, "ReportDeleted");
+
+    truffleAssert.reverts(
+      reportInstance.deleteReport(0, { from: accounts[0] }),
+      "Report has already been deleted"
+    );
   });
 });

f3ac193fba14f7c8ddd4094fde5bd24a91b09843
ydymegan <meganyee1404@gmail.com>
2024-04-21 23:00:24 +0800

    remove unused files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,12 @@
-const Post = artifacts.require('Post');
-const User = artifacts.require('User');
-const Report = artifacts.require('Report');
-const Token = artifacts.require('Token');
-const AcceptanceVoting = artifacts.require('AcceptanceVoting');
+const Post = artifacts.require("Post");
+const User = artifacts.require("User");
+const Report = artifacts.require("Report");
+const AcceptanceVoting = artifacts.require("AcceptanceVoting");
 
 module.exports = function (deployer) {
   deployer
     .deploy(Post)
     .then(() => deployer.deploy(User))
     .then(() => deployer.deploy(Report, Post.address))
-    .then(() => deployer.deploy(Token))
     .then(() => deployer.deploy(AcceptanceVoting, User.address));
 };
